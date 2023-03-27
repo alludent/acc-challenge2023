0d253678e8124c81b957478b6b7e4b138b59c387
alludent <89278803+alludent@users.noreply.github.com>
2023-03-27 15:32:35 -0500

    Add files via upload

---
+++ UI.py
@@ -0,0 +1,53 @@
+
+
+from ursina import *
+
+class UI:
+    def __init__(self, editor_camera, player, gun, grappleGun, resetEnemies):
+        self.editor_camera = editor_camera
+        self.player = player
+        self.gun = gun
+        self.grappleGun = grappleGun
+        self.resetEnemies = resetEnemies
+        
+        pause_handler = Entity(ignore_paused=True, input=self.pause_input)
+
+        self.deathSceneSetup()
+        
+
+    def pause_input(self, key):
+        if key == 'tab':  # press tab to toggle edit/play mode
+            self.editor_camera.enabled = not self.editor_camera.enabled
+
+            self.player.visible_self = self.editor_camera.enabled
+            self.player.cursor.enabled = not self.editor_camera.enabled
+            self.gun.enabled = not self.editor_camera.enabled
+            self.grappleGun.enabled = not self.editor_camera.enabled
+            mouse.locked = not self.editor_camera.enabled
+            self.editor_camera.position = self.player.position
+
+            application.paused = self.editor_camera.enabled
+
+
+    def deathSceneSetup(self):
+        self.death_panel = Panel(scale=2, model='quad')
+        self.death_panel.retry = Button(parent=self.death_panel, color=color.red, position=(0, 0), text = "Retry")
+        self.death_panel.retry.on_click = self.on_respawn
+        self.death_panel.visible = False
+        self.death_panel.retry.enabled = False
+
+
+    def on_respawn(self):
+        print("respawning")
+        self.death_panel.visible = False
+        self.death_panel.retry.enabled = False
+        self.player.hp = 100
+        self.player.position = Vec3(0, .5, -10)
+        self.resetEnemies()
+
+
+    def on_player_death(self):
+        print("u died")
+        self.death_panel.retry.enabled = True
+        self.death_panel.visible = True
+        self.player.cursor.enabled = True
