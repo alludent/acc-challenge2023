33eec5792d3c8d6abb3698a2979fecd14822b8ed
alludent <89278803+alludent@users.noreply.github.com>
2023-04-01 19:04:36 -0500

    pause fix1

--- UI.py
+++ UI.py
@@ -1,4 +1,3 @@
-
 from ursina.prefabs.health_bar import HealthBar
 from ursina import *
 
@@ -10,52 +9,54 @@ class UI:
         self.grappleGun = grappleGun
         self.resetEnemies = resetEnemies
         
-        pause_handler = Entity(ignore_paused=True, input=self.on_pause)
+        
+        self.death_scene_setup()
+        self.pause_menu_setup()
 
-        self.deathSceneSetup()
-        self.pauseMenuSetup()
+        pauseHandler = Entity(ignore_paused=True, input=self.editor_menu)
+        
 
-    def createHealthBar(self):
+    def create_healthbar(self):
         return HealthBar(self.player.hp, bar_color=color.hex("E80000"), roundness=0.5, y= window.top_left[1] - 0.01, scale_y=0.03, scale_x=0.3)
 
-    def on_pause(self, key):
-        if key == 'tab':  # press tab to toggle edit/play mode
-            self.editor_camera.enabled = not self.editor_camera.enabled
+    def set_player_state(self):
+        self.editor_camera.enabled = not self.editor_camera.enabled
 
-            self.player.visible_self = self.editor_camera.enabled
-            self.player.cursor.enabled = not self.editor_camera.enabled
-            self.gun.enabled = not self.editor_camera.enabled
-            self.grappleGun.enabled = not self.editor_camera.enabled
-            mouse.locked = not self.editor_camera.enabled
-            self.editor_camera.position = self.player.position
+        self.player.visible_self = self.editor_camera.enabled
+        self.player.cursor.enabled = not self.editor_camera.enabled
+        self.gun.enabled = not self.editor_camera.enabled
+        self.grappleGun.enabled = not self.editor_camera.enabled
+        mouse.locked = not self.editor_camera.enabled
+        self.editor_camera.position = self.player.position
 
-            application.paused = self.editor_camera.enabled
+        application.paused = self.editor_camera.enabled
+
+    def pause_menu_setup(self):
+        self.pause_menu = Panel(scale=2, model='quad')
             
-            print('pausing')
-            self.pause_menu.retry.enabled = True
-            self.pause_menu.exit.enabled = True
-            self.pause_menu.visible = True
-            
-            
-    def pauseMenuSetup(self):
-        self.pause_menu = Entity( parent=camera.ui, enabled=False, model='quad', scale=(.6, .8), position=(0, 0), 
-                            color=color.rgba(0, 0, 0, 200), origin=(-.5, .5))
-            
-        self.pause_menu.retry = Button( parent=self.pause_menu, text='Restart', position=(0, .1), on_click=self.restart_game)
+        self.pause_menu.retry = Button( parent=self.pause_menu, text='Restart', position=(0, .1), on_click=self.on_respawn)
         self.pause_menu.exit = Button( parent=self.pause_menu, text='Exit', position=(0, -.1), on_click=application.quit)
         self.pause_menu.visible = False
         self.pause_menu.retry.enabled = False
-        self.pause_menu.exit.enabled = False    
+        self.pause_menu.exit.enabled = False  
+        
+        
+    def editor_menu(self, key):
+        if key == 'escape':  # press tab to toggle edit/play mode
+            self.set_player_state()
+            
+            if self.editor_camera.enabled:
+                print('pausing')
+                self.pause_menu.retry.enabled = True
+                self.pause_menu.exit.enabled = True
+                self.pause_menu.visible = True
+            else:
+                print('unpausing')
+                self.pause_menu.retry.enabled = False
+                self.pause_menu.exit.enabled = False
+                self.pause_menu.visible = False
 
-    def restart_game(self):
-        print("restarting")
-        self.pause_menu.visible = False
-        self.pause_menu.retry.enabled = False
-        self.pause_menu.exit.enabled = False
-        self.player.cursor.enabled = False
-
-
-    def deathSceneSetup(self):
+    def death_scene_setup(self):
         self.death_panel = Panel(scale=2, model='quad')
         self.death_panel.retry = Button(parent=self.death_panel, color=color.red, position=(0, 0), text = "Retry")
         self.death_panel.retry.on_click = self.on_respawn
@@ -65,6 +66,11 @@ class UI:
 
     def on_respawn(self):
         print("respawning")
+        self.set_player_state()
+                
+        self.pause_menu.visible = False
+        self.pause_menu.retry.enabled = False
+        self.pause_menu.exit.enabled = False
         self.death_panel.visible = False
         self.death_panel.retry.enabled = False
 
@@ -79,6 +85,4 @@ class UI:
     def on_player_death(self):
         print("u died")
         self.death_panel.retry.enabled = True
-        self.death_panel.visible = True
-        self.player.cursor.enabled = True
-
+        self.death
